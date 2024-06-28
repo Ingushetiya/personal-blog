42326c81920327e8e2d43409d1cf2233b5f48a51
Martazanov <saidmartazanov1@gmail.com>
2024-06-28 19:05:15 +0300

    добавил тесты

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -3,16 +3,18 @@ import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'ru',
-    debug: true,
-    interpolation: {
-      escapeValue: false, 
-    }
-  });
+i18n.use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'ru',
+        debug: true,
+        react: {
+            useSuspense: false,
+        },
+        interpolation: {
+            escapeValue: false,
+        },
+    });
 
 export default i18n;
